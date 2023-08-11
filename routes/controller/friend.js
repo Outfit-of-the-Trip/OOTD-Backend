80e50fc8690b6a5aa88a8555fa4a273f47284f56
unknown <aest3ra>
2023-08-11 15:37:31 +0900

    feat : 친구 요청목록 확인

--- routes/controller/friend.js
+++ routes/controller/friend.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const getMyFriends = require('../service/friendService/getMyFriends')
+const getFriendRequests = require('../service/friendService/getFriendRequests')
 
 router.get('/friends.find_by_userid', async (req, res, next) => {
-    return res.send(JSON.stringify(await getMyFriends.getMyFriends(req.query.userID)));
+    return res.send(await getMyFriends.getMyFriends(req.query.userID));
+});
+
+router.get('/friends.request', async (req, res, next) => {
+    return res.send(await getFriendRequests.getFriendRequests(req.query.userID));
 });
 
 module.exports = router;
