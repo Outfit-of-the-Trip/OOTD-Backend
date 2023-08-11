98aa27a573a36b4fd2db64dee883896dcb10db45
aestera <aest3ra@gmail.com>
2023-08-11 20:36:46 +0900

    ID로 친구 찾기

--- routes/controller/friend.js
+++ routes/controller/friend.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const getMyFriends = require('../service/friendService/getMyFriends')
 const getFriendRequests = require('../service/friendService/getFriendRequests')
 const getFriendAccept = require('../service/friendService/getFriendAccept')
+const findFriend = require('../service/friendService/findFriend')
 
 router.get('/friends.find_by_userid', async (req, res, next) => {
     return res.send(await getMyFriends.getMyFriends(req.query.userID));
@@ -14,6 +15,10 @@ router.get('/friends.request', async (req, res, next) => {
 
 router.get('/friends.accept', async (req, res, next) => {
     return res.send(await getFriendAccept.getFriendAccept(req.query.userID));
+});
+
+router.get('/friends.info', async (req, res, next) => {
+    return res.send(await findFriend.findFriend(req.query.userID));
 });
 
 module.exports = router;
