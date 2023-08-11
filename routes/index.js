ccf1d1ee6bd156822bc98e9089d1d5c942c8cd4a
aestera <aest3ra@gmail.com>
2023-08-11 23:02:52 +0900

    feat : 여행 추가 & 삭제

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 
 const userController = require('./controller/user')
 const friendController = require('./controller/friend')
+const TravelController = require('./controller/travel')
 //const closetController = require('./controller/closet')
 
 router.use('/users', userController)
 router.use('/friends', friendController)
+router.use('/travel', friendController)
 //router.use('/closet', closetController)
 
 module.exports = router
