2f75dd42fcc9d2a31ec8ce856a7fe62e33e68c21
aestera <aest3ra@gmail.com>
2023-08-29 01:24:39 +0900

    refactor : combine Friend Request and Accept in /addFriend

--- routes/service/friendService/addFriend.js
+++ routes/service/friendService/addFriend.js
@@ -1,11 +1,25 @@
 const promisePool = require("../../../config/db")
 
-exports.findFriend = async (body) => {
-    const q = "INSERT INTO FRIENDS (toUsrId, fromUsrId, areWeFriend) values (?, ?, ?), (?, ?, ?)"
-    const data = [body.requestUserId, body.responseUserId, true, body.responseUserId, body.requestUserId, false]
+exports.addFriend = async (body) => {
+    const q = "SELECT * FROM FRIENDS WHERE toUsrId = ? AND fromUsrId = ?"
+
+    const data = [body.responseUserId, body.requestUserId]
+
     try{
-        const [rows, fields] = await promisePool.query(q, data);
-        return {"success": true}
+        const [rows1] = await promisePool.query(q, data);
+        
+        if(!rows1.length){
+            const q1 = "INSERT INTO FRIENDS (toUsrId, fromUsrId, areWeFriend) values (?, ?, ?), (?, ?, ?)"
+            const data1 = [body.requestUserId, body.responseUserId, false, body.responseUserId, body.requestUserId, true]
+            const [rows] = await promisePool.query(q1, data1);
+            return {"msg": "Friend Request SUCCESS"}
+        }
+        else{
+            const updateQuery = "UPDATE FRIENDS SET areWeFriend=1 WHERE fromUsrId = ? AND toUsrId = ?"
+            const data2 = [body.responseUserId, body.requestUserId]
+            const [rows] = await promisePool.query(q, data2);
+            return {"msg": "Friend Accept SUCCESS"}
+        }
     }
     catch{
         return {"success": false}
