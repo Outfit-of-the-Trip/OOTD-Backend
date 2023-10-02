3f2458491e8b713bee73a795123e9aeb19afdf31
aestera <aest3ra@gmail.com>
2023-10-02 19:44:20 +0900

    feat: minor change

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const app = express();
 const bodyParser = require("body-parser");
 const config = require("./config/config")
 const cors = require("cors");
-const fs = require("fs");
-const path = require("path");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -16,27 +14,4 @@ app.use(cors({
 
 app.use('/api', routes);
 
-app.listen(config.PORT, () => {
-    console.log(`[+] Listening on port ${config.PORT}`);
-    const uploadFolder = path.join(__dirname, 'uploads');
-
-    fs.readdir(uploadFolder, (err, files) => {
-        if (err) {
-            console.error('파일 목록을 가져오는 중 오류 발생:', err);
-            return;
-        }
-
-        files.forEach((file) => {
-            const filePath = path.join(uploadFolder, file);
-
-            fs.unlink(filePath, (err) => {
-                if (err) {
-                    console.error(`파일 삭제 중 오류 발생 (${file}):`, err);
-                } else {
-                    console.log(`파일 삭제 완료 (${file})`);
-                }
-            });
-        });
-    });
-
-});
+app.listen(config.PORT, () => console.log(`[+] Listening on port ${config.PORT}`));
--- routes/controller/closet.js
+++ routes/controller/closet.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
 
 const uploadClosetImage = require('../service/closetService/uploadClosetImage')
 const getClosetData = require('../service/closetService/getClosetData')
 
-const upload = multer({ dest: 'uploads/', limits: { fileSize: 5 * 1024 * 1024 } });
 
-
-router.get('/uploadClosetImage', async (req, res, next) => {
-    return res.sendfile('./upload.html');
-});
-
-router.post('/uploadClosetImage', upload.single('img'), async (req, res, next) => {
+router.post('/uploadClosetImage', async (req, res, next) => {
     return res.send(await uploadClosetImage.uploadClosetImage(req.body));
 });
 
